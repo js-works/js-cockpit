25dc8c577c1c26da3eca655bf3a8447dc66954b4
xdev1 <xdev1@users.noreply.github.com>
2021-11-19 07:16:30 +0100

    Tried out themes alternative

--- src/main/misc/__themes.ts
+++ src/main/misc/__themes.ts
@@ -24,7 +24,7 @@ const COLOR_LUMINANCES = [
   0.02 // 950
 ]
 
-const SEMANTIC_COLORS = [
+const SEMANTIC_COLOR_NAMES = [
   'primary',
   'success',
   'info',
@@ -32,46 +32,45 @@ const SEMANTIC_COLORS = [
   'danger'
 ] as const
 
-const PALETTE_COLORS = [
-  'amber',
-  'blue',
-  'blue-gray',
-  'cool-gray',
-  'cyan',
-  'danger',
-  'emerald',
-  'fuchsia',
-  'gray',
-  'green',
-  'indigo',
-  'lime',
-  'neutral',
-  'orange',
-  'pink',
-  'purple',
-  'red',
-  'rose',
-  'sky',
-  'success',
-  'teal',
-  'true-gray',
-  'violet',
-  'warm-gray',
-  'yellow'
-] as const
+const PALETTE_COLORS: Record<string, Color> = {
+  amber: [245, 158, 11],
+  blue: [59, 130, 246],
+  cyan: [6, 182, 212],
+  emerald: [16, 185, 129],
+  fuchsia: [217, 70, 239],
+  gray: [113, 113, 122],
+  green: [34, 197, 94],
+  indigo: [99, 102, 241],
+  lime: [132, 204, 22],
+  orange: [255, 120, 28],
+  pink: [236, 72, 153],
+  purple: [168, 85, 247],
+  red: [239, 68, 68],
+  rose: [244, 63, 94],
+  sky: [14, 165, 233],
+  teal: [20, 184, 166],
+  violet: [139, 92, 246],
+  yellow: [240, 184, 20]
+}
 
-const ALL_COLORS = new Set([...SEMANTIC_COLORS, ...PALETTE_COLORS])
+const PALETTE_COLOR_NAMES = Object.keys(PALETTE_COLORS)
+
+const ALL_COLOR_NAMES = new Set([
+  ...SEMANTIC_COLOR_NAMES,
+  ...PALETTE_COLOR_NAMES
+])
 
 // === types =========================================================
 
+type Color = [red: number, green: number, blue: number]
 type ThemeTokens = typeof lightThemeTokens
 
 type ThemeCustomizing = {
-  primaryColor?: string
-  successColor?: string
-  infoColor?: string
-  warningColor?: string
-  dangerColor?: string
+  primaryColor?: Color
+  successColor?: Color
+  infoColor?: Color
+  warningColor?: Color
+  dangerColor?: Color
   dark?: boolean
 }
 
@@ -92,13 +91,11 @@ class Theme {
 
     const tokens = { ...lightThemeTokens }
 
-    for (const semanticColor of SEMANTIC_COLORS) {
-      const colorHex = customizing[`${semanticColor}Color`]
+    for (const semanticColor of SEMANTIC_COLOR_NAMES) {
+      const color = customizing[`${semanticColor}Color`]
 
-      if (colorHex) {
-        Object.assign(tokens, {
-          ...Theme.#calcColorShades(semanticColor, colorHex)
-        })
+      if (color) {
+        Object.assign(tokens, calcColorShades(semanticColor, color))
       }
     }
 
@@ -154,7 +151,7 @@ class Theme {
     //invertedTokens['color-neutral-0'] = tokens['color-neutral-1000']
     //invertedTokens['color-neutral-1000'] = tokens['color-neutral-0']
 
-    ALL_COLORS.forEach((color) => {
+    ALL_COLOR_NAMES.forEach((color) => {
       for (let i = 0; i < 5; ++i) {
         const key1 = `color-${color}-${i === 0 ? 50 : i * 100}`
         const key2 = `color-${color}-${i === 0 ? 950 : 1000 - i * 100}`
@@ -251,30 +248,30 @@ class Theme {
       'input-height-large': '2.5rem'
     })
   }
-
-  static #calcColorShades(
-    colorName: string,
-    colorHex: string,
-    dark = false
-  ): Record<`color-${string}-${string}`, string> {
-    const ret: any = {}
-
-    COLOR_LUMINANCES.forEach((luminance, idx) => {
-      if (dark) {
-        idx = 1000 - idx
-      }
-
-      ret[`color-${colorName}-${COLOR_SHADES[idx]}`] = calcColor(
-        colorHex,
-        luminance
-      ).join(' ')
-    })
-
-    return ret
-  }
 }
 
 // === public functions ==============================================
+
+function calcColorShades(
+  colorName: string,
+  color: Color,
+  dark = false
+): Record<`color-${string}-${string}`, string> {
+  const ret: any = {}
+
+  COLOR_LUMINANCES.forEach((luminance, idx) => {
+    if (dark) {
+      idx = 1000 - idx
+    }
+
+    ret[`color-${colorName}-${COLOR_SHADES[idx]}`] = calcColor(
+      color,
+      luminance
+    ).join(' ')
+  })
+
+  return ret
+}
 
 function loadTheme(theme: Theme, selector?: string) {
   const elem = document.createElement('style')
@@ -289,119 +286,21 @@ function loadTheme(theme: Theme, selector?: string) {
 // For color naming see: https://chir.ag/projects/name-that-color/#DD5A8C
 
 const Themes = predefineThemes({
-  default: {
-    primaryColor: '#2899e2'
-    //primaryColor: '#0077cB'
-  },
-
-  blue: {
-    primaryColor: '#00B0FF'
-  },
-
-  orange: {
-    primaryColor: '#ff7606'
-  },
-
-  teal: {
-    primaryColor: '#008080'
-  },
-
-  // Error colors
-  // #FFA6C9
-  // #F77703
-  // #FF6B53
-
-  horizon: {
-    primaryColor: '#71d9f2'
-  },
-
-  bostonBlue: {
-    primaryColor: '#45B1E8',
-    dangerColor: '#E34234'
-  },
-
-  pacificBlue: {
-    primaryColor: '#0E94BB',
-    dangerColor: '#E34234'
-  },
-
-  cranberry: {
-    primaryColor: '#DD5A8C'
-  },
-
-  turqoiseBlue: {
-    primaryColor: '#47E3EB'
-  },
-
-  orchid: {
-    primaryColor: '#BF68BD'
-  },
-
-  temp: {
-    primaryColor: '#2889e2' // baseui based
-    //primaryColor: '#3366e2' // baseui based
-    //primaryColor: '#3366cc' // baseui
-    //primaryColor: '#0078d4' // fluentui
-    //primaryColor: '#6595c4' // industrial
-
-    //primaryColor: '#76D7EA' // +
-    // primaryColor: '#6595c4' // bluish
-    // primaryColor: '#c71d7c' // pink++
-    //primaryColor: '#f2704e' // coral?
-    //primaryColor: '#40a6ff' //
-    //primaryColor: '#c05c5c' // indian red 2
-    //primaryColor: '#8b008b' // +
-    //primaryColor: '#b22222' //
-    //primaryColor: '#cd5c5c' // indian red
-    //primaryColor: '#00bbff' // default?
-    //primaryColor: '#cd5c5c' //
-    //primaryColor: '#a9ddff' //
-    //primaryColor: '#dd1493' //
-    //primaryColor: '#7FFFD4' // aquamarine
-    //primaryColor: '#00FFFF' // aqua?
-    //primaryColor: '#008B8B' // cyan
-    //primaryColor: '#00CED1' // dark turquoise
-    //primaryColor: '#008000' // green
-    //primaryColor: '#008080' // teal
-    //primaryColor: '#800080' // purple
-    //primaryColor: '#DA70D6' // orchid ++
-    //primaryColor: '#C71585' // violet red
-    //primaryColor: '#BA55D3' // orchid
-    //primaryColor: '#66CDAA' // aquamarine
-    //primaryColor: '#FF69B4' // +
-    //primaryColor: '#00BFFF' // sky blue
-    //primaryColor: '#FF7F50' // coral
-    //primaryColor: '#7EC0EE' //
-    //primaryColor: '#F660AB' //
-    //primaryColor: '#00bbff' //
-    //primaryColor: '#9c36b5' //
-    //primaryColor: '#4F84C4' // +
-    //primaryColor: '#EA7E5D' // -
-    //primaryColor: '#008b8b' // +
-    //primaryColor: '#00bfff' //
-    //primaryColor: '#5f9ea0' //
-    //primaryColor: '#ff8c00' //
-  },
-
-  skyBlue: {
-    primaryColor: '#0EA5E9'
-  },
-
-  aquamarine: {
-    primaryColor: '#7FFFD4'
-  },
-
-  coral: {
-    primaryColor: '#ff7f50'
-  },
-
-  pink: {
-    primaryColor: '#d24899'
-  },
-
-  turquoise: {
-    primaryColor: '#40e0d0'
-  }
+  default: { primaryColor: [40, 153, 226] },
+  blue: { primaryColor: [0, 176, 255] },
+  orange: { primaryColor: [255, 118, 6] },
+  teal: { primaryColor: [0, 128, 128] },
+  horizon: { primaryColor: [113, 217, 242] },
+  bostonBlue: { primaryColor: [69, 177, 232], dangerColor: [227, 66, 52] },
+  pacificBlue: { primaryColor: [227, 66, 52], dangerColor: [227, 66, 52] },
+  cranberry: { primaryColor: [221, 90, 140] },
+  turqoiseBlue: { primaryColor: [71, 227, 235] },
+  orchid: { primaryColor: [191, 104, 189] },
+  skyBlue: { primaryColor: [14, 165, 233] },
+  aquamarine: { primaryColor: [127, 255, 212] },
+  coral: { primaryColor: [255, 127, 80] },
+  pink: { primaryColor: [210, 72, 153] },
+  turquoise: { primaryColor: [64, 224, 208] }
 })
 
 // === theme helpers =================================================
@@ -428,8 +327,8 @@ function predefineThemes(
 
 // === color utility functions =======================================
 
-function calcColor(hex: string, lum: number): [number, number, number] {
-  let [r, g, b] = hexToRgb(hex)
+function calcColor(color: Color, lum: number): [number, number, number] {
+  let [r, g, b] = color
   let iter = 0
   let l = luminanceOfRgb(r, g, b)
   let rmin = 0
@@ -491,6 +390,12 @@ function luminanceOfValue(x: number): number {
 
 // === Shoelace original light theme =================================
 
+const paletteColorTokens: any = {}
+
+Object.entries(PALETTE_COLORS).forEach(([name, color]) => {
+  Object.assign(paletteColorTokens, calcColorShades(name, color))
+})
+
 const lightThemeTokens = {
   // The following two custom properties will be added
   // automtically to allow features to be turned on and off
@@ -508,303 +413,15 @@ const lightThemeTokens = {
   'label-alignment-aside': 'var(--off)',
   'label-alignment-aside-width': '8rem',
 
-  'color-blue-gray-50': '248 250 252',
-  'color-blue-gray-100': '241 245 249',
-  'color-blue-gray-200': '226 232 240',
-  'color-blue-gray-300': '203 213 225',
-  'color-blue-gray-400': '148 163 184',
-  'color-blue-gray-500': '100 116 139',
-  'color-blue-gray-600': '71 85 105',
-  'color-blue-gray-700': '51 65 85',
-  'color-blue-gray-800': '30 41 59',
-  'color-blue-gray-900': '15 23 42',
-  'color-blue-gray-950': '10 16 30',
-  'color-cool-gray-50': '249 250 251',
-  'color-cool-gray-100': '243 244 246',
-  'color-cool-gray-200': '229 231 235',
-  'color-cool-gray-300': '209 213 219',
-  'color-cool-gray-400': '156 163 175',
-  'color-cool-gray-500': '107 114 128',
-  'color-cool-gray-600': '75 85 99',
-  'color-cool-gray-700': '55 65 81',
-  'color-cool-gray-800': '31 41 55',
-  'color-cool-gray-900': '17 24 39',
-  'color-cool-gray-950': '12 17 29',
-  'color-gray-50': '250 250 250',
-  'color-gray-100': '244 244 245',
-  'color-gray-200': '228 228 231',
-  'color-gray-300': '212 212 216',
-  'color-gray-400': '161 161 170',
-  'color-gray-500': '113 113 122',
-  'color-gray-600': '82 82 91',
-  'color-gray-700': '63 63 70',
-  'color-gray-800': '39 39 42',
-  'color-gray-900': '24 24 27',
-  'color-gray-950': '19 19 22',
-  'color-true-gray-50': '250 250 250',
-  'color-true-gray-100': '245 245 245',
-  'color-true-gray-200': '229 229 229',
-  'color-true-gray-300': '212 212 212',
-  'color-true-gray-400': '163 163 163',
-  'color-true-gray-500': '115 115 115',
-  'color-true-gray-600': '82 82 82',
-  'color-true-gray-700': '64 64 64',
-  'color-true-gray-800': '38 38 38',
-  'color-true-gray-900': '23 23 23',
-  'color-true-gray-950': '17 17 17',
-  'color-warm-gray-50': '250 250 249',
-  'color-warm-gray-100': '245 245 244',
-  'color-warm-gray-200': '231 229 228',
-  'color-warm-gray-300': '214 211 209',
-  'color-warm-gray-400': '168 162 158',
-  'color-warm-gray-500': '120 113 108',
-  'color-warm-gray-600': '87 83 78',
-  'color-warm-gray-700': '68 64 60',
-  'color-warm-gray-800': '41 37 36',
-  'color-warm-gray-900': '28 25 23',
-  'color-warm-gray-950': '19 18 16',
-  'color-red-50': '254 242 242',
-  'color-red-100': '254 226 226',
-  'color-red-200': '254 202 202',
-  'color-red-300': '252 165 165',
-  'color-red-400': '248 113 113',
-  'color-red-500': '239 68 68',
-  'color-red-600': '220 38 38',
-  'color-red-700': '185 28 28',
-  'color-red-800': '153 27 27',
-  'color-red-900': '127 29 29',
-  'color-red-950': '80 20 20',
-  'color-orange-50': '255 247 237',
-  'color-orange-100': '255 237 213',
-  'color-orange-200': '254 215 170',
-  'color-orange-300': '253 186 116',
-  'color-orange-400': '251 146 60',
-  'color-orange-500': '249 115 22',
-  'color-orange-600': '234 88 12',
-  'color-orange-700': '194 65 12',
-  'color-orange-800': '154 52 18',
-  'color-orange-900': '124 45 18',
-  'color-orange-950': '82 32 15',
-  'color-amber-50': '255 251 235',
-  'color-amber-100': '254 243 199',
-  'color-amber-200': '253 230 138',
-  'color-amber-300': '252 211 77',
-  'color-amber-400': '251 191 36',
-  'color-amber-500': '245 158 11',
-  'color-amber-600': '217 119 6',
-  'color-amber-700': '180 83 9',
-  'color-amber-800': '146 64 14',
-  'color-amber-900': '120 53 15',
-  'color-amber-950': '74 35 11',
-  'color-yellow-50': '254 252 232',
-  'color-yellow-100': '254 249 195',
-  'color-yellow-200': '254 240 138',
-  'color-yellow-300': '253 224 71',
-  'color-yellow-400': '250 204 21',
-  'color-yellow-500': '234 179 8',
-  'color-yellow-600': '202 138 4',
-  'color-yellow-700': '161 98 7',
-  'color-yellow-800': '133 77 14',
-  'color-yellow-900': '113 63 18',
-  'color-yellow-950': '60 38 11',
-  'color-lime-50': '247 254 231',
-  'color-lime-100': '236 252 203',
-  'color-lime-200': '217 249 157',
-  'color-lime-300': '190 242 100',
-  'color-lime-400': '163 230 53',
-  'color-lime-500': '132 204 22',
-  'color-lime-600': '101 163 13',
-  'color-lime-700': '77 124 15',
-  'color-lime-800': '63 98 18',
-  'color-lime-900': '54 83 20',
-  'color-lime-950': '38 57 14',
-  'color-green-50': '240 253 244',
-  'color-green-100': '220 252 231',
-  'color-green-200': '187 247 208',
-  'color-green-300': '134 239 172',
-  'color-green-400': '74 222 128',
-  'color-green-500': '34 197 94',
-  'color-green-600': '22 163 74',
-  'color-green-700': '21 128 61',
-  'color-green-800': '22 101 52',
-  'color-green-900': '20 83 45',
-  'color-green-950': '12 49 27',
-  'color-emerald-50': '236 253 245',
-  'color-emerald-100': '209 250 229',
-  'color-emerald-200': '167 243 208',
-  'color-emerald-300': '110 231 183',
-  'color-emerald-400': '52 211 153',
-  'color-emerald-500': '16 185 129',
-  'color-emerald-600': '5 150 105',
-  'color-emerald-700': '4 120 87',
-  'color-emerald-800': '6 95 70',
-  'color-emerald-900': '6 78 59',
-  'color-emerald-950': '3 45 34',
-  'color-teal-50': '240 253 250',
-  'color-teal-100': '204 251 241',
-  'color-teal-200': '153 246 228',
-  'color-teal-300': '94 234 212',
-  'color-teal-400': '45 212 191',
-  'color-teal-500': '20 184 166',
-  'color-teal-600': '13 148 136',
-  'color-teal-700': '15 118 110',
-  'color-teal-800': '17 94 89',
-  'color-teal-900': '19 78 74',
-  'color-teal-950': '12 46 44',
-  'color-cyan-50': '236 254 255',
-  'color-cyan-100': '207 250 254',
-  'color-cyan-200': '165 243 252',
-  'color-cyan-300': '103 232 249',
-  'color-cyan-400': '34 211 238',
-  'color-cyan-500': '6 182 212',
-  'color-cyan-600': '8 145 178',
-  'color-cyan-700': '14 116 144',
-  'color-cyan-800': '21 94 117',
-  'color-cyan-900': '22 78 99',
-  'color-cyan-950': '16 52 66',
-  'color-sky-50': '240 249 255',
-  'color-sky-100': '224 242 254',
-  'color-sky-200': '186 230 253',
-  'color-sky-300': '125 211 252',
-  'color-sky-400': '56 189 248',
-  'color-sky-500': '14 165 233',
-  'color-sky-600': '2 132 199',
-  'color-sky-700': '3 105 161',
-  'color-sky-800': '7 89 133',
-  'color-sky-900': '12 74 110',
-  'color-sky-950': '11 50 73',
-  'color-blue-50': '239 246 255',
-  'color-blue-100': '219 234 254',
-  'color-blue-200': '191 219 254',
-  'color-blue-300': '147 197 253',
-  'color-blue-400': '96 165 250',
-  'color-blue-500': '59 130 246',
-  'color-blue-600': '37 99 235',
-  'color-blue-700': '29 78 216',
-  'color-blue-800': '30 64 175',
-  'color-blue-900': '30 58 138',
-  'color-blue-950': '21 33 73',
-  'color-indigo-50': '238 242 255',
-  'color-indigo-100': '224 231 255',
-  'color-indigo-200': '199 210 254',
-  'color-indigo-300': '165 180 252',
-  'color-indigo-400': '129 140 248',
-  'color-indigo-500': '99 102 241',
-  'color-indigo-600': '79 70 229',
-  'color-indigo-700': '67 56 202',
-  'color-indigo-800': '55 48 163',
-  'color-indigo-900': '49 46 129',
-  'color-indigo-950': '36 33 84',
-  'color-violet-50': '245 243 255',
-  'color-violet-100': '237 233 254',
-  'color-violet-200': '221 214 254',
-  'color-violet-300': '196 181 253',
-  'color-violet-400': '167 139 250',
-  'color-violet-500': '139 92 246',
-  'color-violet-600': '124 58 237',
-  'color-violet-700': '109 40 217',
-  'color-violet-800': '91 33 182',
-  'color-violet-900': '76 29 149',
-  'color-violet-950': '49 21 88',
-  'color-purple-50': '250 245 255',
-  'color-purple-100': '243 232 255',
-  'color-purple-200': '233 213 255',
-  'color-purple-300': '216 180 254',
-  'color-purple-400': '192 132 252',
-  'color-purple-500': '168 85 247',
-  'color-purple-600': '147 51 234',
-  'color-purple-700': '126 34 206',
-  'color-purple-800': '107 33 168',
-  'color-purple-900': '88 28 135',
-  'color-purple-950': '47 17 67',
-  'color-fuchsia-50': '253 244 255',
-  'color-fuchsia-100': '250 232 255',
-  'color-fuchsia-200': '245 208 254',
-  'color-fuchsia-300': '240 171 252',
-  'color-fuchsia-400': '232 121 249',
-  'color-fuchsia-500': '217 70 239',
-  'color-fuchsia-600': '192 38 211',
-  'color-fuchsia-700': '162 28 175',
-  'color-fuchsia-800': '134 25 143',
-  'color-fuchsia-900': '112 26 117',
-  'color-fuchsia-950': '56 16 58',
-  'color-pink-50': '253 242 248',
-  'color-pink-100': '252 231 243',
-  'color-pink-200': '251 207 232',
-  'color-pink-300': '249 168 212',
-  'color-pink-400': '244 114 182',
-  'color-pink-500': '236 72 153',
-  'color-pink-600': '219 39 119',
-  'color-pink-700': '190 24 93',
-  'color-pink-800': '157 23 77',
-  'color-pink-900': '131 24 67',
-  'color-pink-950': '67 14 35',
-  'color-rose-50': '255 241 242',
-  'color-rose-100': '255 228 230',
-  'color-rose-200': '254 205 211',
-  'color-rose-300': '253 164 175',
-  'color-rose-400': '251 113 133',
-  'color-rose-500': '244 63 94',
-  'color-rose-600': '225 29 72',
-  'color-rose-700': '190 18 60',
-  'color-rose-800': '159 18 57',
-  'color-rose-900': '136 19 55',
-  'color-rose-950': '74 13 32',
-  'color-primary-50': '240 249 255',
-  'color-primary-100': '224 242 254',
-  'color-primary-200': '186 230 253',
-  'color-primary-300': '125 211 252',
-  'color-primary-400': '56 189 248',
-  'color-primary-500': '14 165 233',
-  'color-primary-600': '2 132 199',
-  'color-primary-700': '3 105 161',
-  'color-primary-800': '7 89 133',
-  'color-primary-900': '12 74 110',
-  'color-primary-950': '11 50 73',
-  'color-success-50': '240 253 244',
-  'color-success-100': '220 252 231',
-  'color-success-200': '187 247 208',
-  'color-success-300': '134 239 172',
-  'color-success-400': '74 222 128',
-  'color-success-500': '34 197 94',
-  'color-success-600': '22 163 74',
-  'color-success-700': '21 128 61',
-  'color-success-800': '22 101 52',
-  'color-success-900': '20 83 45',
-  'color-success-950': '12 49 27',
-  'color-warning-50': '255 251 235',
-  'color-warning-100': '254 243 199',
-  'color-warning-200': '253 230 138',
-  'color-warning-300': '252 211 77',
-  'color-warning-400': '251 191 36',
-  'color-warning-500': '245 158 11',
-  'color-warning-600': '217 119 6',
-  'color-warning-700': '180 83 9',
-  'color-warning-800': '146 64 14',
-  'color-warning-900': '120 53 15',
-  'color-warning-950': '74 35 11',
-  'color-danger-50': '254 242 242',
-  'color-danger-100': '254 226 226',
-  'color-danger-200': '254 202 202',
-  'color-danger-300': '252 165 165',
-  'color-danger-400': '248 113 113',
-  'color-danger-500': '239 68 68',
-  'color-danger-600': '220 38 38',
-  'color-danger-700': '185 28 28',
-  'color-danger-800': '153 27 27',
-  'color-danger-900': '127 29 29',
-  'color-danger-950': '80 20 20',
-  'color-neutral-50': '250 250 250',
-  'color-neutral-100': '244 244 245',
-  'color-neutral-200': '228 228 231',
-  'color-neutral-300': '212 212 216',
-  'color-neutral-400': '161 161 170',
-  'color-neutral-500': '113 113 122',
-  'color-neutral-600': '82 82 91',
-  'color-neutral-700': '63 63 70',
-  'color-neutral-800': '39 39 42',
-  'color-neutral-900': '24 24 27',
-  'color-neutral-950': '19 19 22',
+  // TODO - optimize as color shades are already calculated
+  ...calcColorShades('primary', [14, 165, 233]),
+  ...calcColorShades('success', [34, 197, 94]),
+  ...calcColorShades('warning', [245, 158, 11]),
+  ...calcColorShades('danger', [239, 68, 68]),
+  ...calcColorShades('neutral', [113, 113, 122]),
+
+  ...paletteColorTokens,
+
   'color-neutral-0': '255 255 255',
   'color-neutral-1000': '0 0 0',
   'border-radius-small': '0.125rem',
