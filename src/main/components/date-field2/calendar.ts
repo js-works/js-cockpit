0758e6099612720ee994eb4f9c8ba11cd76960af
xdev1 <xdev1.github@gmail.com>
2022-08-27 01:01:42 +0200

    Continued work on date picker

--- src/main/components/date-field2/calendar.ts
+++ src/main/components/date-field2/calendar.ts
@@ -18,7 +18,6 @@ namespace Calendar {
     locale: string;
     selectionMode: SelectionMode;
     highlightWeekend: boolean;
-    onSelection: (selection: Date[]) => void;
   };
 
   export type SelectionMode =
@@ -36,24 +35,20 @@ namespace Calendar {
 // === local data ====================================================
 
 const defaultLocale = 'en-US';
-
-const defaultOptions: Calendar.Options = {
-  locale: 'en',
-  selectionMode: 'date',
-  highlightWeekend: true,
-  onSelection: () => {}
-};
-
+const noWeekends: [number, number] = [-1, -1];
 const airLocaleDataCache = new Map<string, AirDatepickerLocale>();
 
 // === class Calendar ================================================
 
 class Calendar {
-  #options = defaultOptions;
   #elem = document.createElement('div');
   #input: HTMLInputElement;
   #picker!: AirDatepicker;
-  #baseAirOptions: Partial<AirDatepickerOptions>;
+
+  #options: Partial<AirDatepickerOptions> = {};
+  #locale = defaultLocale;
+  #selectionMode: Calendar.SelectionMode = 'date';
+  #i18n = new I18nFacade(() => this.#locale);
 
   static #styles = `
     :host {
@@ -81,8 +76,6 @@ class Calendar {
       padding: 0.3rem 0;
     }
 
-    
-
     .adp-date-input {
       position: absolute;
       opacity: 0;
@@ -101,10 +94,12 @@ class Calendar {
     .-weekend- {
       background-color: var(--adp-background-color-highlight);
     }
-    
   `;
 
-  constructor(options?: Partial<Calendar.Options>) {
+  constructor(
+    onSelection: (selection: Date[]) => void,
+    customStyles: string = ''
+  ) {
     const datepickerStyleElem = document.createElement('style');
     const auxStyleElem = document.createElement('style');
     const inputElem = document.createElement('input');
@@ -122,125 +117,133 @@ class Calendar {
 
     this.#elem.className = 'adp-base';
     datepickerStyleElem.innerText = datepickerStyles;
-    auxStyleElem.innerText = Calendar.#styles;
+    auxStyleElem.innerText = Calendar.#styles + '\n' + customStyles;
     inputElem.className = 'adp-date-input';
-    pickerContainerElem.className = 'adp-picker-container';
 
-    this.#baseAirOptions = {
+    this.#update({
       inline: true,
       container: pickerContainerElem,
-      keyboardNav: true
-    };
+      keyboardNav: true,
+      locale: getAirLocaleData(defaultLocale),
+      weekends: noWeekends,
 
-    //this.#picker = new AirDatepicker(inputElem, this.#baseAirOptions);
-
-    this.setOptions(options || defaultOptions);
+      onSelect: ({ date }) =>
+        onSelection(
+          Array.isArray(date) ? date : date === undefined ? [] : [date]
+        )
+    });
   }
 
   getElement() {
     return this.#elem;
   }
 
-  mergeOptions(options: Partial<Calendar.Options>) {
-    this.#options = { ...this.#options, ...options };
+  setLocale(locale: string) {
+    if (this.#locale !== locale) {
+      return;
+    }
+
+    const options = {
+      locale: getAirLocaleData(locale)
+    };
+
+    this.#locale = locale;
+    this.#update(options);
   }
 
-  setOptions(options: Partial<Calendar.Options>) {
-    this.#options = { ...defaultOptions, ...options };
-    const airOptions = {
-      ...convertOptions(this.#options),
-      ...this.#baseAirOptions
-    };
+  setSelectionMode(selectionMode: Calendar.SelectionMode) {
+    if (this.#selectionMode === selectionMode) {
+      return;
+    }
+
+    const options: Partial<AirDatepickerOptions> = {};
+
+    switch (selectionMode) {
+      case 'date':
+        options.multipleDates = false;
+        options.view = 'days';
+        options.timepicker = false;
+        options.onlyTimepicker = false;
+        options.minView = 'days';
+        break;
+
+      case 'dates':
+        options.multipleDates = true;
+        options.view = 'days';
+        options.timepicker = false;
+        options.onlyTimepicker = false;
+        options.minView = 'days';
+        break;
+
+      case 'time':
+        options.multipleDates = false;
+        options.timepicker = true;
+        options.onlyTimepicker = true;
+        break;
+
+      case 'dateTime':
+        options.multipleDates = false;
+        options.view = 'days';
+        options.timepicker = true;
+        options.onlyTimepicker = false;
+        options.minView = 'days';
+        break;
+
+      case 'dateRange':
+        options.multipleDates = false;
+        options.range = true;
+        options.view = 'days';
+        options.timepicker = false;
+        options.onlyTimepicker = false;
+        options.minView = 'days';
+        break;
+
+      case 'month':
+        options.timepicker = false;
+        options.onlyTimepicker = false;
+        options.minView = 'months';
+        options.view = 'months';
+        break;
+
+      case 'year':
+        options.timepicker = false;
+        options.onlyTimepicker = false;
+        options.view = 'years';
+        options.minView = 'years';
+        break;
+    }
+
+    this.#selectionMode = selectionMode;
+    this.#update(options);
+  }
+
+  setHighlightWeekends(value: boolean) {
+    if (value === false && this.#options.weekends === noWeekends) {
+      return;
+    }
+
+    this.#update({
+      weekends: (value ? this.#i18n.getWeekendDays() : noWeekends) as any
+    });
+  }
+
+  #update = (options: Partial<AirDatepickerOptions>) => {
+    this.#options = { ...this.#options, ...options };
 
     if (this.#picker) {
       this.#picker.destroy();
+      (this.#options.container as HTMLElement).innerHTML = '';
     }
 
-    this.#picker = new AirDatepicker(this.#input, airOptions);
+    this.#picker = new AirDatepicker(this.#input, this.#options);
 
     this.#picker.$datepicker.addEventListener('click', () => {
       this.#input.focus();
     });
-  }
+  };
 }
 
 // === helpers =======================================================
-
-function convertOptions(
-  options: Calendar.Options
-): Partial<AirDatepickerOptions> {
-  const ret: Partial<AirDatepickerOptions> = {};
-
-  ret.locale = getAirLocaleData(options.locale || defaultLocale);
-
-  if (options.onSelection) {
-    const onSelect = options.onSelection;
-    ret.onSelect = ({ date }) => onSelect(Array.isArray(date) ? date : [date]);
-  }
-
-  switch (options.selectionMode) {
-    case 'date':
-      ret.multipleDates = false;
-      ret.view = 'days';
-      ret.timepicker = false;
-      ret.onlyTimepicker = false;
-      ret.minView = 'days';
-      break;
-
-    case 'dates':
-      ret.multipleDates = true;
-      ret.view = 'days';
-      ret.timepicker = false;
-      ret.onlyTimepicker = false;
-      ret.minView = 'days';
-      break;
-
-    case 'time':
-      ret.multipleDates = false;
-      ret.timepicker = true;
-      ret.onlyTimepicker = true;
-      break;
-
-    case 'dateTime':
-      ret.multipleDates = false;
-      ret.view = 'days';
-      ret.timepicker = true;
-      ret.onlyTimepicker = false;
-      ret.minView = 'days';
-      break;
-
-    case 'dateRange':
-      ret.multipleDates = false;
-      ret.range = true;
-      ret.view = 'days';
-      ret.timepicker = false;
-      ret.onlyTimepicker = false;
-      ret.minView = 'days';
-      break;
-
-    case 'month':
-      ret.timepicker = false;
-      ret.onlyTimepicker = false;
-      ret.minView = 'months';
-      ret.view = 'months';
-      break;
-
-    case 'year':
-      ret.timepicker = false;
-      ret.onlyTimepicker = false;
-      ret.view = 'years';
-      ret.minView = 'years';
-      break;
-  }
-
-  if (options.highlightWeekend) {
-    ret.weekends = new I18nFacade(() => options.locale).getWeekendDays() as any;
-  } else {
-    ret.weekends = [-1, -1];
-  }
-
-  return ret;
-}
 
 function getAirLocaleData(locale: string): AirDatepickerLocale {
   let airLocaleData = airLocaleDataCache.get(locale);
--- src/main/components/date-field2/date-field2.ts
+++ src/main/components/date-field2/date-field2.ts
@@ -41,12 +41,11 @@ export class DateField2 extends Component {
   constructor() {
     super();
 
-    this.#calendar = new Calendar({
-      locale: this.#i18n.getLocale(),
-      onSelection(selection) {},
-      selectionMode: 'date',
-      highlightWeekend: true
-    });
+    this.#calendar = new Calendar((sel) => {
+      console.log('selection: ', sel);
+    }, getDatepickerStyles());
+
+    this.#calendar.setLocale(this.#i18n.getLocale());
   }
 
   render() {
@@ -79,4 +78,61 @@ export class DateField2 extends Component {
       </div>
     `;
   }
+}
+
+function getDatepickerStyles() {
+  return `
+    .air-datepicker {
+      --adp-font-family: var(--sl-font-sans);
+      --adp-font-size: 14px;
+      --adp-width: 246px;
+      --adp-z-index: 100;
+      --adp-padding: 4px;
+      --adp-grid-areas: 'nav' 'body' 'timepicker' 'buttons';
+      --adp-transition-duration: .3s;
+      --adp-transition-ease: ease-out;
+      --adp-transition-offset: 8px;
+      --adp-background-color: #fff;
+      --adp-background-color-hover: #f0f0f0;
+      --adp-background-color-active: #eaeaea;
+      --adp-background-color-in-range: rgba(92, 196, 239, .1);
+      --adp-background-color-in-range-focused: rgba(92, 196, 239, .2);
+      --adp-background-color-selected-other-month-focused: #8ad5f4;
+      --adp-background-color-selected-other-month: #a2ddf6;
+      --adp-color: #4a4a4a;
+      --adp-color-secondary: #9c9c9c;
+      --adp-accent-color: var(--sl-color-primary-500);
+      --adp-color-current-date: var(--adp-accent-color);
+      --adp-color-other-month: #dedede;
+      --adp-color-disabled: #aeaeae;
+      --adp-color-disabled-in-range: #939393;
+      --adp-color-other-month-hover: #c5c5c5;
+      --adp-border-color: #dbdbdb;
+      --adp-border-color-inner: #efefef;
+      --adp-border-radius: 4px;
+      --adp-border-color-inline: #d7d7d7;
+      --adp-nav-height: 32px;
+      --adp-nav-arrow-color: var(--adp-color-secondary);
+      --adp-nav-action-size: 32px;
+      --adp-nav-color-secondary: var(--adp-color-secondary);
+      --adp-day-name-color: var(--sl-color-neutral-500);
+      --adp-day-name-color-hover: #8ad5f4;
+      --adp-day-cell-width: 1fr;
+      --adp-day-cell-height: 32px;
+      --adp-month-cell-height: 42px;
+      --adp-year-cell-height: 56px;
+      --adp-pointer-size: 10px;
+      --adp-poiner-border-radius: 2px;
+      --adp-pointer-offset: 14px;
+      /* --adp-cell-border-radius: 4px; */
+      --adp-cell-background-color-selected: #5cc4ef;
+      --adp-cell-background-color-selected-hover: #45bced;
+      --adp-cell-background-color-in-range: rgba(92, 196, 239, 0.1);
+      --adp-cell-background-color-in-range-hover: rgba(92, 196, 239, 0.2);
+      --adp-cell-border-color-in-range: var(--adp-cell-background-color-selected);
+      --adp-btn-height: 32px;
+      --adp-btn-color: var(--adp-accent-color);
+      --adp-btn-color-hover: var(--adp-color);
+    };
+  `;
 }
