7eed80605d0344746711eeea3bd163851d562bc1
xdev1 <xdev1.github@gmail.com>
2022-09-03 19:10:51 +0200

    Continued work on cal

--- src/main/utils/cal.ts
+++ src/main/utils/cal.ts
@@ -140,6 +140,10 @@ class Calendar {
   #currMonth: number;
   #currYear: number;
   #currDecade: number;
+  #currHour: number | null = 0;
+  #currMinute: number | null = 0;
+
+  #refreshRequested = false;
 
   constructor(params: {
     getLocaleSettings: (locale: string) => Partial<Calendar.LocaleSettings>;
@@ -178,7 +182,29 @@ class Calendar {
     this.#cal.addEventListener('mousedown', this.#onMouseDown);
     this.#cal.addEventListener('click', this.#onClick);
 
-    this.#refresh();
+    this.#calHour.addEventListener('input', (ev) => {
+      const target = ev.target;
+
+      if (!(target instanceof HTMLInputElement)) {
+        return;
+      }
+
+      this.#currHour = target.valueAsNumber;
+      this.#updateTimeText();
+    });
+
+    this.#calMinute.addEventListener('input', (ev) => {
+      const target = ev.target;
+
+      if (!(target instanceof HTMLInputElement)) {
+        return;
+      }
+
+      this.#currMinute = target.valueAsNumber;
+      this.#updateTimeText();
+    });
+
+    this.#requestRefresh();
   }
 
   getElement() {
@@ -195,13 +221,85 @@ class Calendar {
   #onClick = (ev: MouseEvent) => {
     const target = ev.target;
 
-    const action =
-      target instanceof HTMLElement
-        ? target.getAttribute('data-action') || null
-        : null;
+    if (this.#refreshRequested || !(target instanceof HTMLElement)) {
+      return;
+    }
+
+    if (target.classList.contains('cal-cell')) {
+      switch (this.#currView) {
+        case 'month':
+          break;
+
+        case 'year':
+          this.#currYear = parseInt(target.getAttribute('data-year')!, 10);
+          this.#currMonth = parseInt(target.getAttribute('data-month')!, 10);
+          this.#currView = 'month';
+          this.#requestRefresh();
+          break;
+
+        case 'decade':
+          this.#currYear = parseInt(target.getAttribute('data-year')!, 10);
+          this.#currView = 'year';
+          this.#requestRefresh();
+          break;
+      }
+
+      return;
+    }
+
+    const action = target.getAttribute('data-action');
 
     if (action) {
       switch (action) {
+        case 'movePrev':
+          if (this.#currView === 'month') {
+            if (this.#currMonth === 0) {
+              this.#currMonth = 11;
+              this.#currYear--;
+            } else {
+              this.#currMonth--;
+            }
+
+            this.#requestRefresh();
+          }
+          if (this.#currView === 'year') {
+            this.#currYear--;
+
+            this.#requestRefresh();
+          } else if (this.#currView === 'decade') {
+            this.#currDecade -= 10;
+            this.#requestRefresh();
+          }
+          break;
+
+        case 'moveNext':
+          if (this.#currView === 'month') {
+            if (this.#currMonth === 11) {
+              this.#currMonth = 0;
+              this.#currYear++;
+            } else {
+              this.#currMonth++;
+            }
+
+            this.#requestRefresh();
+          } else if (this.#currView === 'year') {
+            this.#currYear++;
+
+            this.#requestRefresh();
+          } else if (this.#currView === 'decade') {
+            this.#currDecade += 10;
+            this.#requestRefresh();
+          }
+          break;
+
+        case 'switchView':
+          if (this.#currView !== 'decade') {
+            this.#currView = this.#currView === 'month' ? 'year' : 'decade';
+            this.#requestRefresh();
+          }
+
+          break;
+
         case 'cancel':
           this.#cancel();
           break;
@@ -215,6 +313,19 @@ class Calendar {
           break;
       }
     }
+  };
+
+  #requestRefresh = () => {
+    if (this.#refreshRequested) {
+      return;
+    }
+
+    this.#refreshRequested = true;
+
+    requestAnimationFrame(() => {
+      this.#refreshRequested = false;
+      this.#refresh();
+    });
   };
 
   #refresh = () => {
@@ -246,6 +357,26 @@ class Calendar {
         this.#calMain.innerHTML = '';
         this.#calMain.append(this.#renderDecadeView(this.#currDecade));
     }
+
+    this.#updateTimeText();
+  };
+
+  #updateTimeText = () => {
+    let timeText: string;
+
+    if (this.#currHour === null || this.#currMinute === null) {
+      timeText = '--:--';
+    } else {
+      const hour =
+        this.#currHour < 9 ? `0${this.#currHour}` : `${this.#currHour}`;
+
+      const minute =
+        this.#currMinute < 9 ? `0${this.#currMinute}` : `${this.#currMinute}`;
+
+      timeText = `${hour}:${minute}`;
+    }
+
+    this.#calTime.innerText = timeText;
   };
 
   #clear = () => {};
@@ -282,22 +413,38 @@ class Calendar {
     }
 
     for (let i = 0; i < daysToShow; ++i) {
-      let day: number;
+      let cellYear: number;
+      let cellMonth: number;
+      let cellDay: number;
 
       if (i < remainingDaysOfLastMonth) {
-        day = dayCountOfLastMonth - remainingDaysOfLastMonth + i + 1;
+        cellDay = dayCountOfLastMonth - remainingDaysOfLastMonth + i + 1;
+        cellMonth = month === 0 ? 11 : month - 1;
+        cellYear = month === 0 ? year - 1 : year;
       } else {
-        day = i - remainingDaysOfLastMonth + 1;
+        cellDay = i - remainingDaysOfLastMonth + 1;
 
-        if (day > dayCountOfCurrMonth) {
-          day = day - dayCountOfCurrMonth;
+        if (cellDay > dayCountOfCurrMonth) {
+          cellDay = cellDay - dayCountOfCurrMonth;
+          cellMonth = month === 11 ? 1 : month + 1;
+          cellYear = month === 11 ? year + 1 : year;
+        } else {
+          cellMonth = month;
+          cellYear = year;
         }
       }
 
       if (i % 7 === 0) {
-        const weekElem = h('div', {
-          className: 'cal-week-number'
-        });
+        const weekElem = h(
+          'div',
+          {
+            className: 'cal-week-number'
+          },
+          getCalendarWeek(
+            new Date(cellYear, cellMonth, cellDay),
+            this.#localeSettings.firstDayOfWeek
+          )
+        );
 
         ret.append(weekElem);
       }
@@ -306,11 +453,11 @@ class Calendar {
         'div',
         {
           'className': 'cal-cell',
-          'data-year': year,
-          'data-month': month,
-          'data-day': day
+          'data-year': cellYear,
+          'data-month': cellMonth,
+          'data-day': cellDay
         },
-        day
+        cellDay
       );
 
       ret.append(elem);
@@ -323,16 +470,14 @@ class Calendar {
     const ret = h('div', { className: 'cal-view-year' });
 
     for (let i = 0; i < 12; ++i) {
-      const month = this.#localeSettings.monthsShort[i];
-
       const elem = h(
         'div',
         {
           'className': 'cal-cell',
           'data-year': year,
-          'data-month': month
+          'data-month': i
         },
-        month
+        this.#localeSettings.monthsShort[i]
       );
 
       ret.append(elem);
@@ -375,13 +520,24 @@ function getDayCountOfMonth(year: number, month: number) {
   return year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0) ? 28 : 29;
 }
 
-function getFirstWeekdayInMonth(year: number, month: number) {
-  const n = year * 12 + month;
+function getCalendarWeek(date: Date, firstDayOfWeek: number) {
+  // Code is based on this solution here:
+  // https://stackoverflow.com/questions/23781366/date-get-week-number-for-custom-week-start-day
+  // TODO - check algorithm
 
-  year = Math.floor(n / 12);
-  month = n % 12;
+  const target = new Date(date);
 
-  return new Date(year, month, 1).getDay();
+  // Replaced offset of (6) with (7 - firstDayOfWeek)
+  const dayNum = (date.getDay() + 7 - firstDayOfWeek) % 7;
+  target.setDate(target.getDate() - dayNum + 3);
+  const firstThursday = target.valueOf();
+  target.setMonth(0, 1);
+
+  if (target.getDay() !== 4) {
+    target.setMonth(0, 1 + ((4 - target.getDay() + 7) % 7));
+  }
+
+  return 1 + Math.ceil((firstThursday - target.valueOf()) / 604800000);
 }
 
 function query<T extends HTMLElement>(root: HTMLElement, selector: string): T {
@@ -458,13 +614,13 @@ function renderRoot(theme: Calendar.Theme): HTMLDivElement {
       <div class="cal-nav">
         <a class="cal-prev" data-action="movePrev">&#x1F860;</a>
         <div class="cal-title-container">
-          <a class="cal-title" data-action="switchView">[title]</a>
+          <a class="cal-title" data-action="switchView"></a>
         </div>
         <a class="cal-next" data-action="moveNext">&#x1F862;</a>
       </div>
       <div class="cal-main">[main]</div>
       <div class="cal-time-selector">
-        <div class="cal-time">--:--</div>
+        <div class="cal-time"></div>
         <input class="cal-hour" value="0" type="range" min="0" max="23" />
         <input class="cal-minute" value="0" type="range" min="0" max="59" />
       </div>
